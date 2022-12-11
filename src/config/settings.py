0332c33bd715b5d3844cfef8eeb215b9514e4ee3
son nguyen <son.nguyen2@encapital.io>
2022-12-12 01:02:33 +0700

    update db

--- src/config/settings.py
+++ src/config/settings.py
@@ -77,13 +77,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'user-authen',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '45.32.103.46',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
